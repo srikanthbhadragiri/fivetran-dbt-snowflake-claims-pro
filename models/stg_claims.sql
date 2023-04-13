e2b9fc30d5a3bcfbf3f7cf74b110377493cc8f6c
srikanth.bhadragiri@gmail.com <srikanth.bhadragiri@gmail.com>
2023-04-13 08:12:26 +0000

    Modularized code into 3 staging .sql files and used {{ref()}} to call the staging files.

---
+++ models/stg_claims.sql
@@ -0,0 +1,6 @@
+with
+clm as (
+    select claim_id, submission_date, gross, patientshare, net, vat
+    from raw_sc.claim_master
+)
+select * from clm
---
+++ models/stg_diagnosis.sql
@@ -0,0 +1,7 @@
+with
+diag as (
+    select claim_id, count(diagnosis_code) total_diagnosis
+    from raw_sc.claim_diagnosis
+    group by claim_id
+)
+select * from diag
